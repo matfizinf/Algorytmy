24f23a33956b659e5ef453d870974531ca2e5388
informatyk <informatyk@gmail.com>
2025-07-21 16:13:50 +0200

    Utworzenie Folderu Szyfrowanie przestawieniowe i dodanie rozwiązania zadania 1

---
+++ Algorytmy na tekstach/Szyfrowanie przestawieniowe/zad1.py
@@ -0,0 +1,38 @@
+def szyfrowanie_kolumnowe1(n, s, w, k):
+    T = []
+    t = 0
+    wynik = []
+    for i in range(w):
+        wiersz = []
+        for j in range(k):
+            if t < n:
+                wiersz.append(s[t])
+                t += 1
+            else:
+                wiersz.append('X')
+        T.append(wiersz)
+
+    for i in range(k):
+        for j in range(w):
+            wynik.append(T[j][i])
+
+    return ''.join(wynik)
+
+s = 'ala ma kota i psa'
+n = len(s)
+w = 5
+k = 4
+print(szyfrowanie_kolumnowe1(n, s, w, k))
+
+def szyfrowanie_kolumnowe2(n, s, w, k):
+    for i in range(n, w * k):
+        s = s + 'X'
+
+    wynik = []
+    for i in range(k):
+        for j in range(w):
+            wynik.append(s[j * k + i])
+
+    return ''.join(wynik)
+
+print(szyfrowanie_kolumnowe2(n, s, w, k))
